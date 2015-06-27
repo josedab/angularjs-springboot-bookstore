0e9159e6ee657cd3d33fd3d59562f383aed462b4
Jose David Baena <josedab@gmail.com>
2015-06-27 12:48:49 -0700

    Gatling simulation for Author stress testing

---
+++ src/test/gatling/simulations/AuthorGatlingTest.scala
@@ -0,0 +1,104 @@
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+
+import _root_.io.gatling.core.scenario.Simulation
+import ch.qos.logback.classic.{Level, LoggerContext}
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.duration._
+
+/**
+ * Performance test for the Author entity.
+ */
+class AuthorGatlingTest extends Simulation {
+
+    val context: LoggerContext = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+    // Log all HTTP requests
+    //context.getLogger("io.gatling.http").setLevel(Level.valueOf("TRACE"))
+    // Log failed HTTP requests
+    //context.getLogger("io.gatling.http").setLevel(Level.valueOf("DEBUG"))
+
+    val baseURL = Option(System.getProperty("baseURL")) getOrElse """http://127.0.0.1:8080"""
+
+    val httpConf = http
+        .baseURL(baseURL)
+        .inferHtmlResources()
+        .acceptHeader("*/*")
+        .acceptEncodingHeader("gzip, deflate")
+        .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+        .connection("keep-alive")
+        .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0")
+
+    val headers_http = Map(
+        "Accept" -> """application/json"""
+    )
+
+        val authorization_header = "Basic " + Base64.getEncoder.encodeToString("bookstoreapp:mySecretOAuthSecret".getBytes(StandardCharsets.UTF_8))
+
+    val headers_http_authentication = Map(
+        "Content-Type" -> """application/x-www-form-urlencoded""",
+        "Accept" -> """application/json""",
+        "Authorization"-> authorization_header
+    )
+
+    val headers_http_authenticated = Map(
+        "Accept" -> """application/json""",
+        "Authorization" -> "Bearer ${access_token}"
+    )
+
+    val scn = scenario("Test the Author entity")
+        .exec(http("First unauthenticated request")
+        .get("/api/account")
+        .headers(headers_http)
+        .check(status.is(401)))
+        .pause(10)
+        .exec(http("Authentication")
+        .post("/oauth/token")
+        .headers(headers_http_authentication)
+        .formParam("username", "admin")
+        .formParam("password", "admin")
+        .formParam("grant_type", "password")
+        .formParam("scope", "read write")
+        .formParam("client_secret", "mySecretOAuthSecret")
+        .formParam("client_id", "bookstoreapp")
+        .formParam("submit", "Login")
+        .check(jsonPath("$.access_token").saveAs("access_token")))
+        .pause(1)
+        .exec(http("Authenticated request")
+        .get("/api/account")
+        .headers(headers_http_authenticated)
+        .check(status.is(200)))
+        .pause(10)
+        .repeat(2) {
+            exec(http("Get all authors")
+            .get("/api/authors")
+            .headers(headers_http_authenticated)
+            .check(status.is(200)))
+            .pause(10 seconds, 20 seconds)
+            .exec(http("Create new author")
+            .put("/api/authors")
+            .headers(headers_http_authenticated)
+            .body(StringBody("""{"id":null, "name":"SAMPLE_TEXT", "surname":"SAMPLE_TEXT", "description":"SAMPLE_TEXT", "birthDate":"2020-01-01T00:00:00.000Z"}""")).asJSON
+            .check(status.is(201))
+            .check(headerRegex("Location", "(.*)").saveAs("new_author_url")))
+            .pause(10)
+            .repeat(5) {
+                exec(http("Get created author")
+                .get("${new_author_url}")
+                .headers(headers_http_authenticated))
+                .pause(10)
+            }
+            .exec(http("Delete created author")
+            .delete("${new_author_url}")
+            .headers(headers_http_authenticated))
+            .pause(10)
+        }
+
+    val users = scenario("Users").exec(scn)
+
+    setUp(
+        users.inject(rampUsers(100) over (1 minutes))
+    ).protocols(httpConf)
+}
