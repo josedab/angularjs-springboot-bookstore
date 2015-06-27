8f223bf51504d848d1dd71f57fe9e2b97a6dcd6a
Jose David Baena <josedab@gmail.com>
2015-06-27 12:44:33 -0700

    Author backend updates - Controllers, repository and resources for the Author entity

---
+++ src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,13 @@
+package com.bookstore.repository;
+
+import com.bookstore.domain.Author;
+import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the Author entity.
+ */
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+
+}
